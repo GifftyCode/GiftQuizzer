21b4a0a1c1469029ff06b2a5395abd0e1b4e9923
Gifftybabe <ulokangozi@gmail.com>
2024-09-28 05:57:01 +0100

    Home layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,35 +1,34 @@
-import React from 'react'
+
+import React from 'react';
+import { Helmet } from 'react-helmet';
 import { Link } from 'react-router-dom';
 
-import { Helmet } from 'react-helmet'
 import Icon from '@mdi/react';
 import { mdiCubeOutline } from '@mdi/js';
 
-const Home = () => {
-  return (
+const Home = () => (
     <>
-    <Helmet><title>GiftQuizzer</title></Helmet>
-    <div id="home">
-        <section>
-        <div style={{ textAlign: 'center' }}>
-      <Icon path={mdiCubeOutline} size={4} color='orange' />
-    </div>
-            <h1>GiftQuizzer</h1>
-            <div className="play-button-container">
-                <ul>
-                    <li >
-                    <Link className='play-button' to="/play/instructions" rel="noopener noreferrer">Play</Link>
-                    </li>
-                </ul>
-            </div>
-            <div className="auth-container">
-                <Link to="/login" className='auth-button' id='login-button'>Login</Link>
-                <Link to="/register" className='auth-button' id='signup-button'>Register</Link>
-            </div>
-        </section>
-    </div>
+        <Helmet><title>Home - GiftQuizzer</title></Helmet>
+        <div id="home">
+            <section>
+                <div style={{ textAlign: 'center' }}>
+                <div style={{ textAlign: 'center' }}>
+                    <Icon path={mdiCubeOutline} size={4} color='orange' />
+                </div>
+                </div>
+                <h1>GiftQuizzer</h1>
+                <div className="play-button-container">
+                    <ul>
+                        <li><Link className="play-button" to="/play/instructions">Play</Link></li>
+                    </ul>
+                </div>
+                <div className="auth-container">
+                    <Link to="/login" className="auth-buttons" id="login-button">Login</Link>
+                    <Link to="/register" className="auth-buttons" id="signup-button">Sign up</Link>
+                </div>
+            </section>
+        </div>
     </>
-  )
-}
+);
 
-export default Home
+export default Home;
