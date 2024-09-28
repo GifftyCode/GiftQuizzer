8a888cda8a5e98ce3f12ee83af033a4a5c0b956c
Gifftybabe <ulokangozi@gmail.com>
2024-09-28 05:25:23 +0100

    app jsx file update

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 
 import Home from './components/Home';
+import QuizInstructions from './components/quiz/QuizInstructions';
 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" exact element={<Home />} />
+        <Route path="/play/instructions" exact element={<QuizInstructions />} />
      
       </Routes>
     </Router>
