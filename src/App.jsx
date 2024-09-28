4d8cb1f704cecef755d8ef5891c5a970648e10d8
Gifftybabe <ulokangozi@gmail.com>
2024-09-28 01:46:03 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+
+import Home from './components/Home';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
      
-    </>
-  )
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
