bf1c65fecdc1958be69862747a9e8a99c3da6735
Gifftybabe <ulokangozi@gmail.com>
2024-09-28 04:10:11 +0100

    main jsx update

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+// import '../node_modules/@mdi/font/css/materialdesignicons.min.css';
+// import '../node_modules/materialize-css/dist/css/materialize.min.css';
+// import '../node_modules/materialize-css/dist/js/materialize.min.js';
+
 import App from './App.jsx'
 import './styles/styles.scss'
 
